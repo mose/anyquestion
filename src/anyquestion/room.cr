6ace1bc91a8dc28697c3d6abeb2939a43f7fc9ef
mose <mose@gandi.net>
2016-06-15 13:34:40 +0800

    gimme some debug

--- src/anyquestion/room.cr
+++ src/anyquestion/room.cr
@@ -11,7 +11,9 @@ module Anyquestion
     def initialize(name)
       @name = name
       @time_start = Time.now
+      puts @time_start
       @id = @time_start.to_s("%s").to_i + Random.new.rand(1000)
+      puts @id
       @questions = {} of Int32 => Question
       @sockets = [] of HTTP::WebSocket
     end
