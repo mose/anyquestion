b218b514c42d67efe5d5c5ed694bc11972085448
mose <mose@mose.com>
2016-06-23 19:01:57 +0800

    tuning

--- src/anyquestion/sessions.cr
+++ src/anyquestion/sessions.cr
@@ -8,13 +8,13 @@ module Anyquestion
     def set(env, value)
       key = Random.new.rand(500000)
       @all[key] = value
-      c = HTTP::Cookie.new(@session_name, value, "/", Time.now.to_utc + Time::Span.new(4, 0, 0))
+      c = HTTP::Cookie.new(@session_name, key, "/", Time.now.to_utc + Time::Span.new(4, 0, 0))
       c.add_response_headers env.response.headers
     end
 
     def check?(env, value)
-      if env.request.cookies[@session_name]? && @all[env.request.cookies[@session_name].value]
-        true
+      if env.request.cookies[@session_name]? && @all[env.request.cookies[@session_name].value]?
+        @all[env.request.cookies[@session_name].value] == value
       else
         false
       end
