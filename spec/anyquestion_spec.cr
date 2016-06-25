5047dc538e163cda4b58be34418ee7d31ab47b20
mose <mose@mose.com>
2016-06-25 11:22:35 +0800

    starting to poke tests

--- spec/anyquestion_spec.cr
+++ spec/anyquestion_spec.cr
@@ -1,9 +1,14 @@
 require "./spec_helper"
 
-describe Anyquestion do
+describe "Anyquestion web" do
   # TODO: Write tests
 
   it "works" do
     false.should eq(true)
+  end
+
+  it "loads homepage" do
+    client_response = http_request("GET", "/")
+    client_response.body.should match /Any Question/
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,15 @@
 require "spec"
 require "../src/anyquestion"
+
+require "http"
+
+def http_request(method, endpoint)
+  request = HTTP::Request.new(method, endpoint)
+  io = MemoryIO.new
+  response = HTTP::Server::Response.new(io)
+  context = HTTP::Server::Context.new(request, response)
+  Kemal::RouteHandler::INSTANCE.call(context)
+  response.close
+  io.rewind
+  HTTP::Client::Response.from_io(io, decompress: false)
+end
