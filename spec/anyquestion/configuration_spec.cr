0de1cb9d5cfea8c91fab0a60e34a4e9b780d895a
mose <mose@mose.com>
2016-06-26 00:52:34 +0800

    fix test

--- spec/anyquestion/configuration_spec.cr
+++ spec/anyquestion/configuration_spec.cr
@@ -4,9 +4,17 @@ require "../../src/anyquestion/configuration"
 describe Anyquestion::Configuration do
   describe ".config_path" do
     it "by default gives config dist file" do
+      expected = File.expand_path "config/config.dist.yml"
+      config = File.expand_path "config/config.yml"
+      moved = File.expand_path "config/config.bak.yml"
+      if File.exists? config
+        File.rename config, moved
+      end
       path = Anyquestion::Configuration.config_path
-      expected = File.expand_path "config/config.yml"
       path.should eq expected
+      if File.exists? moved
+        File.rename moved, config
+      end
     end
   end
 end
