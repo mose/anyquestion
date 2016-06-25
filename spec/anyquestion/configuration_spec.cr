a840ef43bd048833a2c989df3c2d37cf56a1386a
mose <mose@mose.com>
2016-06-25 21:38:47 +0800

    fix display on import

---
+++ spec/anyquestion/configuration_spec.cr
@@ -0,0 +1,5 @@
+require "../spec_helper"
+require "../../src/anyquestion/configuration"
+
+describe Anyquestion::Configuration do
+end
--- src/anyquestion/configuration.cr
+++ src/anyquestion/configuration.cr
@@ -1,3 +1,5 @@
+require "./config"
+
 module Anyquestion
   class Configuration
     def self.load
