fa03c189d9a0284bb11607996df846bc7275d91a
mose <mose@mose.com>
2016-06-29 00:13:12 +0800

    prepare more tests

---
+++ spec/anyquestion/registry_spec.cr
@@ -0,0 +1,7 @@
+require "../spec_helper"
+require "../../src/anyquestion/question"
+require "../../src/anyquestion/talk"
+require "../../src/anyquestion/registry"
+
+describe Anyquestion::Registry do
+end
---
+++ spec/anyquestion/talk_spec.cr
@@ -0,0 +1,6 @@
+require "../spec_helper"
+require "../../src/anyquestion/question"
+require "../../src/anyquestion/talk"
+
+describe Anyquestion::Talk do
+end
